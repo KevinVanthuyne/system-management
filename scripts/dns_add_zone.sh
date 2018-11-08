ef0a4e4cb43e8b40c5c030637b2ae66708cb7ff7
Kevin Vanthuyne <kevin.vanthuyne2@gmail.com>
2018-11-08 12:58:53 +0000

    add a zone file to bind/zones

--- scripts/dns_add_zone.sh
+++ scripts/dns_add_zone.sh
@@ -7,5 +7,29 @@ echo "zone \"$1.kevin-vanthuyne.sb.uclllabs.be\" {
         allow-transfer { 193.191.176.254; 193.191.177.4; };
         file \"/etc/bind/zones/$1.kevin-vanthuyne.sb.uclllabs.be\";
 };
-" >> test
+" >> /etc/bind/named.conf.local
+
+# add zone file
+echo ";
+; BIND data for $1.kevin-vanthuyne.sb.uclllabs.be
+;
+\$TTL 300 ; 5 min
+@       IN      SOA     ns.kevin-vanthuyne.sb.uclllabs.be. admin.kevin-vanthuyne.sb.uclllabs.be. (
+                        1       ; SERIAL
+                        3h      ; Refresh
+                        1h      ; Retry
+                        1w      ; Expire
+                        300 )    ; Minimum
+;
+@       IN      NS      ns.kevin-vanthuyne.sb.uclllabs.be.
+@       IN      NS      ns1.uclllabs.be.
+@       IN      NS      ns2.uclllabs.be.
+; @     IN      NS      ns.arne-vandebemdt.sb.uclllabs.be.
+
+ns.kevin-vanthuyne.sb.uclllabs.be.      IN      A       193.191.177.161
+;ns1.uclllabs.be.                     IN      A
+;ns2.uclllabs.be.                     IN      A
+;ns.arnevandebemdt.sb.uclllabs.be.
+$1.kevin-vanthuyne.sb.uclllabs.be.      IN      A       193.191.177.161
+" > /etc/bind/zones/$1.kevin-vanthuyne.sb.uclllabs.be
 
