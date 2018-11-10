3be418d02e07f08e2562ea4b3f29aff43cb53f22
Kevin Vanthuyne <kevin.vanthuyne2@gmail.com>
2018-11-10 13:33:43 +0000

    added processing of CNAME records

--- scripts/dns_add_record.sh
+++ scripts/dns_add_record.sh
@@ -16,22 +16,37 @@ while getopts 't:' OPTION; do
 	esac
 done
 
-# check arguments
-if [ "$#" -ne "3" ]
+# process CNAME record
+if [ $type = "CNAME" ]
 then
-	echo "incorrect amount of arguments"
-	echo "script usage: [-t (A|MX|CNAME)] subzone ip-adress zone"
-	exit 1
+	if [ "$#" -ne "2"]
+	then
+		echo "CNAME only takes 2 arguments: -t CNAME alias zone"
+		exit 1
+	else
+		alias=$1
+		zone=$2
+		echo "$alias	IN	CNAME	$zone" >> /etc/bind/zones/$zone
+		echo "added CNAME record '$alias' to $zone"
+	fi
+
+# process other types
+else
+	# check arguments
+	if [ "$#" -ne "3" ]
+	then
+		echo "incorrect amount of arguments"
+		echo "script usage: [-t (A|MX|CNAME)] subzone ip-adress zone"
+		exit 1
+	fi
+
+	subzone=$1
+	ip=$2
+	zone=$3
+
+	echo "added $type record '$subzone' (resolving to $ip) to $zone"
 fi
-
-subzone=$1
-ip=$2
-zone=$3
-
-# add record to zone file
-echo "$subzone.$zone.	IN	$type	$ip" >> /etc/bind/zones/$zone
 
 # find the line with the version number and increase it by 1
 awk -i inplace '/SERIAL/ {gsub(/[0-9]*/, $1+=1, $1)}; { print }' /etc/bind/zones/$zone
 
-echo "added $type record '$subzone' (resolving to $ip) to $zone"
