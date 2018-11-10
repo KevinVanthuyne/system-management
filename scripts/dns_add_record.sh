90a0dcc9fa2495b1213939b00960a81c1d87ab5d
Kevin Vanthuyne <kevin.vanthuyne2@gmail.com>
2018-11-10 10:39:29 +0000

    added -t option

--- scripts/dns_add_record.sh
+++ scripts/dns_add_record.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
-echo "adding record to zone $1"
+type="A"
+
+# parsing options
+while getopts 't:' OPTION; do
+	case "$OPTION" in
+		t)
+			type=$OPTARG
+			;;
+		?)
+			echo "script usage: [-t (A|MX|CNAME)] subzone ip-adress zone"
+			exit 1
+			;;
+	esac
+done
+
+echo "type was: $type"
 
 # find the line with the version number and increase it by 1
-awk -i inplace '/SERIAL/ {gsub(/[0-9]/, $1+=1, $1)}; { print }' $1
+#awk -i inplace '/SERIAL/ {gsub(/[0-9]/, $1+=1, $1)}; { print }' $1
