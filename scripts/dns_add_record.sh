5acaeab8fffa3c433ea5402216f70adc417bcc70
Kevin Vanthuyne <kevin.vanthuyne2@gmail.com>
2018-11-10 10:25:22 +0000

    increase version number when adding record

--- scripts/dns_add_record.sh
+++ scripts/dns_add_record.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-# find the line with the version number
-awk '/SERIAL/ {print $1}' $1
+echo "adding record to zone $1"
+
+# find the line with the version number and increase it by 1
+awk -i inplace '/SERIAL/ {gsub(/[0-9]/, $1+=1, $1)}; { print }' $1
